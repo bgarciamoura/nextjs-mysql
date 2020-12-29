e773df6d5364596d144f72f3d12bb0d492322bfc
Bruno Garcia <bgarciamoura@gmail.com>
2020-12-29 13:35:19 -0300

    Created docker-compose file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingSlash: true,
+    env: {
+        base_api_url: 'http://localhost:3000/api',
+        base_pokemon_url: 'https://pokeapi.co/api/v2',
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
